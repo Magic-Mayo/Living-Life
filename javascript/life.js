2382d32474f976c31d84c5aba4ca0ff5472343c3
Magic-Mayo <mike.mayoii@gmail.com>
2019-06-30 22:16:11 -0700

    added 3 more card bodies in HTML for eaach of the tabs so you can switch between them. wrote function for getting temperature data for each day of the month for the previous year to calculate averages. need to find out why average function isn't seeing the length of the arrays input into the callback to make it work

--- javascript/life.js
+++ javascript/life.js
@@ -1,42 +1,183 @@
+let janMaxTemp = []
+let febMaxTemp = []
+let marMaxTemp = []
+let aprMaxTemp = []
+let mayMaxTemp = []
+let junMaxTemp = []
+let julMaxTemp = []
+let augMaxTemp = []
+let sepMaxTemp = []
+let octMaxTemp = []
+let novMaxTemp = []
+let decMaxTemp = []
+
+let janMinTemp = []
+let febMinTemp = []
+let marMinTemp = []
+let aprMinTemp = []
+let mayMinTemp = []
+let junMinTemp = []
+let julMinTemp = []
+let augMinTemp = []
+let sepMinTemp = []
+let octMinTemp = []
+let novMinTemp = []
+let decMinTemp = []
+
+function average(input){
+    console.log(input)
+    console.log(input.length)
+    const i = arr.reduce((a,b)=>a+b, 0);
+    console.log(i)
+    return i/arr.length;
+}
+
 $(document).on('click', '.search-btn', function(event){
     event.preventDefault();
-
-    // const queryURL = 'https://api.worldweatheronline.com/premium/v1/past-weather.ashx/?format=json&date=2018-02-01&enddate=2018-02-28&key=800def3bb80c42488da184817192906&q=' + city
-    // $.ajax({
-    //     url: queryURL,
-    //     method: 'GET'})
-    //     .then(function(response){
-    //         console.log(response)
-    //     })
-    //     $('#search').val('');
-    // })
-    
-const city = $('#search').val().trim()
-const urls = [`'https://api.worldweatheronline.com/premium/v1/past-weather.ashx/?format=json&date=2018-01-01&enddate=2018-01-31&
-    key=800def3bb80c42488da184817192906&q='` + city,
-    'https://api.worldweatheronline.com/premium/v1/past-weather.ashx/?format=json&date=2018-02-01&enddate=2018-02-28&key=800def3bb80c42488da184817192906&q=' + city,
-    'https://api.worldweatheronline.com/premium/v1/past-weather.ashx/?format=json&date=2018-03-01&enddate=2018-03-30&key=800def3bb80c42488da184817192906&q=' + city,
-    'https://api.worldweatheronline.com/premium/v1/past-weather.ashx/?format=json&date=2018-04-01&enddate=2018-04-31&key=800def3bb80c42488da184817192906&q=' + city,
-    'https://api.worldweatheronline.com/premium/v1/past-weather.ashx/?format=json&date=2018-05-01&enddate=2018-05-30&key=800def3bb80c42488da184817192906&q=' + city,
-    'https://api.worldweatheronline.com/premium/v1/past-weather.ashx/?format=json&date=2018-06-01&enddate=2018-06-30&key=800def3bb80c42488da184817192906&q=' + city,
-    'https://api.worldweatheronline.com/premium/v1/past-weather.ashx/?format=json&date=2018-07-01&enddate=2018-07-31&key=800def3bb80c42488da184817192906&q=' + city,
-    'https://api.worldweatheronline.com/premium/v1/past-weather.ashx/?format=json&date=2018-08-01&enddate=2018-08-31&key=800def3bb80c42488da184817192906&q=' + city,
-    'https://api.worldweatheronline.com/premium/v1/past-weather.ashx/?format=json&date=2018-09-01&enddate=2018-09-30&key=800def3bb80c42488da184817192906&q=' + city,
-    'https://api.worldweatheronline.com/premium/v1/past-weather.ashx/?format=json&date=2018-10-01&enddate=2018-10-31&key=800def3bb80c42488da184817192906&q=' + city,
-    'https://api.worldweatheronline.com/premium/v1/past-weather.ashx/?format=json&date=2018-11-01&enddate=2018-11-30&key=800def3bb80c42488da184817192906&q=' + city,
-    'https://api.worldweatheronline.com/premium/v1/past-weather.ashx/?format=json&date=2018-12-01&enddate=2018-12-31&key=800def3bb80c42488da184817192906&q=' + city];
-
-$.each(urls, function(u){ 
-     $.ajax(u, 
-       {url: u,
+    const city = $('#search').val().trim();
+    const jan = '&date=2018-01-01&enddate=2018-01-31';
+    const feb = '&date=2018-02-01&enddate=2018-02-28';
+    const mar = '&date=2018-03-01&enddate=2018-03-31';
+    const apr = '&date=2018-04-01&enddate=2018-04-30';
+    const may = '&date=2018-05-01&enddate=2018-05-31';
+    const jun = '&date=2018-06-01&enddate=2018-06-30';
+    const jul = '&date=2018-07-01&enddate=2018-07-31';
+    const aug = '&date=2018-08-01&enddate=2018-08-31';
+    const sep = '&date=2018-09-01&enddate=2018-09-30';
+    const oct = '&date=2018-10-01&enddate=2018-10-31';
+    const nov = '&date=2018-11-01&enddate=2018-11-30';
+    const dec = '&date=2018-12-01&enddate=2018-12-31';
+    const queryURL = 'https://api.worldweatheronline.com/premium/v1/past-weather.ashx/?format=json&key=800def3bb80c42488da184817192906&q=' + city;
+    const ajaxJan = $.ajax({
+        url: queryURL + jan,
         method: 'GET',
-       })
-       .then(function(response){
-        console.log(response)
+        async: true,
+        });
+    const ajaxFeb = $.ajax({
+        url: queryURL + feb,
+        method: 'GET',
+        async: true,
+        });
+    const ajaxMar = $.ajax({
+        url: queryURL + mar,
+        method: 'GET',
+        async: true,
+        });
+    const ajaxApr = $.ajax({
+        url: queryURL + apr,
+        method: 'GET',
+        async: true,
+        });
+    const ajaxMay = $.ajax({
+        url: queryURL + may,
+        method: 'GET',
+        async: true,
+        });
+    const ajaxJun = $.ajax({
+        url: queryURL + jun,
+        method: 'GET',
+        async: true,
+        });
+    const ajaxJul = $.ajax({
+        url: queryURL + jul,
+        method: 'GET',
+        async: true,
+        });
+    const ajaxAug = $.ajax({
+        url: queryURL + aug,
+        method: 'GET',
+        async: true,
+        });
+    const ajaxSep = $.ajax({
+        url: queryURL + sep,
+        method: 'GET',
+        async: true,
+        });
+    const ajaxOct = $.ajax({
+        url: queryURL + oct,
+        method: 'GET',
+        async: true,
+        });
+    const ajaxNov = $.ajax({
+        url: queryURL + nov,
+        method: 'GET',
+        async: true,
+        });
+    const ajaxDec = $.ajax({
+        url: queryURL + dec,
+        method: 'GET',
+        async: true,
+        });
+
+    $.when(ajaxJan, ajaxFeb, ajaxMar, ajaxApr, ajaxMay, ajaxJun, ajaxJul, ajaxAug, ajaxSep, ajaxOct, ajaxNov, ajaxDec).then(function(janTemp,febTemp,marTemp,aprTemp,mayTemp,junTemp,julTemp,augTemp,sepTemp,octTemp,novTemp,decTemp){
+
+        for (let i=0; i<janTemp[0].data.weather.length; i++){
+            janMaxTemp.push(janTemp[0].data.weather[i].maxtempF);
+            janMinTemp.push(janTemp[0].data.weather[i].mintempF);
+        }
+
+        for (let i=0; i<febTemp[0].data.weather.length; i++){
+            febMaxTemp.push(febTemp[0].data.weather[i].maxtempF);
+            febMinTemp.push(febTemp[0].data.weather[i].mintempF);
+        }
+
+        for (let i=0; i<marTemp[0].data.weather.length; i++){
+            marMaxTemp.push(marTemp[0].data.weather[i].maxtempF);
+            marMinTemp.push(marTemp[0].data.weather[i].mintempF);
+        }
+
+        for (let i=0; i<aprTemp[0].data.weather.length; i++){
+            aprMaxTemp.push(aprTemp[0].data.weather[i].maxtempF);
+            aprMinTemp.push(aprTemp[0].data.weather[i].mintempF);
+        }
+        
+        for (let i=0; i<mayTemp[0].data.weather.length; i++){
+            mayMaxTemp.push(mayTemp[0].data.weather[i].maxtempF);
+            mayMinTemp.push(mayTemp[0].data.weather[i].mintempF);
+        }
+
+        for (let i=0; i<junTemp[0].data.weather.length; i++){
+            junMaxTemp.push(junTemp[0].data.weather[i].maxtempF);
+            junMinTemp.push(junTemp[0].data.weather[i].mintempF);
+        }
+
+        for (let i=0; i<julTemp[0].data.weather.length; i++){
+            julMaxTemp.push(julTemp[0].data.weather[i].maxtempF);
+            julMinTemp.push(julTemp[0].data.weather[i].mintempF);
+        }
+
+        for (let i=0; i<augTemp[0].data.weather.length; i++){
+            augMaxTemp.push(augTemp[0].data.weather[i].maxtempF);
+            augMinTemp.push(augTemp[0].data.weather[i].mintempF);
+        }
+
+        for (let i=0; i<sepTemp[0].data.weather.length; i++){
+            sepMaxTemp.push(sepTemp[0].data.weather[i].maxtempF);
+            sepMinTemp.push(sepTemp[0].data.weather[i].mintempF);
+        }
+
+        for (let i=0; i<octTemp[0].data.weather.length; i++){
+            octMinTemp.push(parseInt(octTemp[0].data.weather[i].mintempF));
+            octMaxTemp.push(parseInt(octTemp[0].data.weather[i].maxtempF));
+        }
+
+        for (let i=0; i<novTemp[0].data.weather.length; i++){
+            novMaxTemp.push(novTemp[0].data.weather[i].maxtempF);
+            novMinTemp.push(novTemp[0].data.weather[i].mintempF);
+        }
+
+        for (let i=0; i<decTemp[0].data.weather.length; i++){
+            decMaxTemp.push(decTemp[0].data.weather[i].maxtempF);
+            decMinTemp.push(decTemp[0].data.weather[i].mintempF);
+        }
+        // console.log(octTemp[0].data.weather[0].mintempF)
+        // console.log(octTemp)
     })
-$('#search').val('');
-$('#collapseOne').removeClass('show')
-})
+
+    // console.log(octMinTemp)
+    $('#search').val('');
+    $('#collapseOne').removeClass('show')
+    average(octMinTemp)
 })
 
-// Make search not collapsed on page load and collapse on search(add click to search on card header after bar is collapsed)
+// Make on click for jobs, weather, crime, and neighborhood tabs so it will switch between them
