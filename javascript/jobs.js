a5f26ed458e2ca4219593b2b8adb1a9b8c3fdb07
reb2039607 <reb.aguayo@gmail.com>
2019-07-02 20:57:36 -0700

    gets the objects to display, need text to show, jobs are more randomized using new api link

--- javascript/jobs.js
+++ javascript/jobs.js
@@ -2,12 +2,15 @@ $(document).on("click", ".search-btn", function () {
     event.preventDefault();
 
     let city = $("#search").val().trim()
-    const queryURL = "https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=3c9055a0&app_key=b277b839fa391e86d53c328291a45814&where=" + city // Place names or postal codes may be used.
+    const queryURL = "https://api.adzuna.com:443/v1/api/jobs/us/search/1?app_id=3c9055a0&app_key=b277b839fa391e86d53c328291a45814&results_per_page=15&where=" + city + " &distance=16.0934&sort_by=date&full_time=1" // Place names or postal codes may be used.
 
+
+    console.log(city);
 
     $.ajax({
         url: queryURL,
         method: "GET"
+
     })
         .then(function (response) {
 
@@ -15,18 +18,22 @@ console.log(response)//testing response...it works and pulls info from api
 
 //now for the rest...
 
+//creating variable for job results
+// const jobResults = response.results[0].title; //yay!!! the little wins :) 
+// console.log(jobResults);
 
+// $("#jobs").append(jobResults);
 
+let results = response.results;
+console.log(results);
 
-// const jobResults = response.data;
+for(let i = 0; i < results.length; i++) {
+    
+    let localJobs = $("<ul>").text(results[i]); 
+    $("#jobs").append(localJobs);
+}
 
-// for(let i = 0; i < jobResults; i++) {
-// // let results = jobResults[i];
-// let jobDiv = $("<div>"); 
-// // let localJobs = $("<ul>").text(jobResults[i]); 
-// jobDiv.attr("data-type", jobResults[i]);
-// // jobDiv.append("#job-tab");
-
+// jobResults.attr(); ****can I get the redirect_url to be a link when clicking the job title?
 
 // console.log(jobDiv);
 
