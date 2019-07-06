82abfd1f25517501bca4e2b8924e39738a7f4f7e
Brett Schnaidt <brettschnaidt@gmail.com>
2019-07-06 11:19:00 -0700

    appended results to tab

--- javascript/life.js
+++ javascript/life.js
@@ -1,48 +1,30 @@
-<<<<<<< HEAD
-
-let place = 'Chicago'.replace(' ', '+');
-
-
-let queryURL = 'https://api.eventful.com/json/events/search?app_key=tBrJGCvpbKcsx2jS&keywords=location=' +  place + '&date=Future&page_size=15'
-$.ajax({
-    url: queryURL,
-    method: "GET",
-
-
-}).then(function(response){
-    const res = JSON.parse(response);
-    console.log(res);
-    console.log(res.events);
-}) 
-=======
 let highs = [];
 let lows = [];
 
 $(document).on('click', '.search-btn', function(event){
-    event.preventDefault();
-    const city = $('#search').val().trim();
-    const queryURL = 'https://api.worldweatheronline.com/premium/v1/weather.ashx/?format=json&key=80c4283111cd411388653908190107&mca=yes&cc=yes&q=' + city;
-    $.ajax({
-        url: queryURL,
-        method: 'GET'
-    }).then(function(response){
-        console.log(response.data)
-        const average = response.data.ClimateAverages[0].month
-        $('#weather').empty();
-        for (let i=0; i<average.length;i++){
-            highs.push(Math.round(average[i].absMaxTemp_F));
-            lows.push(Math.round(average[i].avgMinTemp_F));
-        }
-
-        for (let i=0; i<highs.length; i++){
-            const weather = $('<div>');
-            weather.append('High: ' + highs[i], ' Low: ' + lows[i]).addClass('temps');
-            $('#weather').append(weather);
-        }
+    // event.preventDefault();
+    // const city = $('#search').val().trim();
+    // const queryURL = 'https://api.worldweatheronline.com/premium/v1/weather.ashx/?format=json&key=80c4283111cd411388653908190107&mca=yes&cc=yes&q=' + city;
+    // $.ajax({
+    //     url: queryURL,
+    //     method: 'GET'
+    // }).then(function(response){
+    //     $('#collapseOne').removeClass('show')
+    //     console.log(response.data)
+    //     const average = response.data.ClimateAverages[0].month
+    //     $('#weather').empty();
+    //     for (let i=0; i<average.length;i++){
+    //         highs.push(Math.round(average[i].absMaxTemp_F));
+    //         lows.push(Math.round(average[i].avgMinTemp_F));
+    //     }
         
-        console.log(highs)
-    })
-        $('#search').val('');
-        $('#collapseOne').removeClass('show')
+    //     for (let i=0; i<highs.length; i++){
+    //         const weather = $('<div>');
+    //         weather.append('High: ' + highs[i], ' Low: ' + lows[i]).addClass('temps');
+    //         $('#weather').append(weather);
+    //     }
+        
+    //     console.log(highs)
+    // })
+        //$('#search').val('');
 })
->>>>>>> Test
