52660636fba2153d8eb75bffa4927d7a13747866
Brett Schnaidt <brettschnaidt@gmail.com>
2019-07-06 11:38:57 -0700

    changed javascript

--- javascript/event.js
+++ javascript/event.js
@@ -1,38 +1,38 @@
-$(document).on('click','.search-btn', function(event){
+$(document).on('click', '.search-btn', function (event) {
     ///Decide which tab we are in this ONE place and then invoke the correct function
     //Tosolve this not in righ way but sort of
     //We can ensure we are on the correct tab before executing any code
     const isEventsTabSelected = $('a[class="nav-link active"]').text().trim() === 'Events';
-  
-    if(isEventsTabSelected){
-    event.preventDefault();
-    const city = $('#search').val().trim();
-    console.log('OUt city',city, $('#search'))
-    
-    let queryURL = 'https://api.eventful.com/json/events/search?app_key=tBrJGCvpbKcsx2jS&location=' +  city + '&date=Future&page_size=15'
-    $.ajax({
-        url: queryURL,
-        method: "GET",
-        contentType: 'application/json; charset=utf-8',
-        crossDomain: true,
-        dataType: 'jsonp'
-        
-    }).then(function(response){
-        const res = response;
-        console.log(res.events.event);
-        // const eventLocal = JSON.parse(response.events.event)
-        // console.log(eventLocal);
 
-        for (let i=0;i<res.events.event.length;i++){
-            const newDiv = $('<div>');
-            const pEl = $('<p>').text(res.events.event[i].description);
-            const hEl = $('<h1>').text(res.events.event[i].title);
-            newDiv.append(hEl).append(pEl);
-            $('#events').append(newDiv);
-        }
-        
-    }) 
+    if (isEventsTabSelected) {
+        event.preventDefault();
+        const city = $('#search').val().trim();
+        console.log('OUt city', city, $('#search'))
+
+        let queryURL = 'https://api.eventful.com/json/events/search?app_key=tBrJGCvpbKcsx2jS&location=' + city + '&date=Future&page_size=15'
+        $.ajax({
+            url: queryURL,
+            method: "GET",
+            contentType: 'application/json; charset=utf-8',
+            crossDomain: true,
+            dataType: 'jsonp'
+
+        }).then(function (response) {
+            const res = response;
+            console.log(res.events.event);
+            // const eventLocal = JSON.parse(response.events.event)
+            // console.log(eventLocal);
+
+            for (let i = 0; i < res.events.event.length; i++) {
+                const newDiv = $('<div>');
+                const pEl = $('<p>').text(res.events.event[i].description);
+                const hEl = $('<h1>').text(res.events.event[i].title);
+                newDiv.append(hEl).append(pEl);
+                $('#events').append(newDiv);
+            }
+
+        })
 
 
-}
+    }
 })
