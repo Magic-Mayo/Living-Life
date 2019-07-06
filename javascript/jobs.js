ad5c63dcc5a7befd237127e12df1eb9a8dbd54ee
reb2039607 <reb.aguayo@gmail.com>
2019-07-06 12:58:11 -0700

    updated and made into collapsible showing job info

--- javascript/jobs.js
+++ javascript/jobs.js
@@ -1,5 +1,6 @@
 $(document).on("click", ".search-btn", function () {
     event.preventDefault();
+    $("#jobs").empty();
 
     let city = $("#search").val().trim()
     const queryURL = "https://api.adzuna.com:443/v1/api/jobs/us/search/1?app_id=3c9055a0&app_key=b277b839fa391e86d53c328291a45814&results_per_page=15&where=" + city + " &distance=16.0934&sort_by=date&full_time=1" // Place names or postal codes may be used.
@@ -16,40 +17,48 @@ $(document).on("click", ".search-btn", function () {
 
 console.log(response)//testing response...it works and pulls info from api
 
-//now for the rest...
-
-//creating variable for job results
-// const jobResults = response.results[0].title; //yay!!! the little wins :) 
-// console.log(jobResults);
-
-// $("#jobs").append(jobResults);
-
+//new variable to use
 let results = response.results;
 console.log(results);
 
+//looping the results
 for(let i = 0; i < results.length; i++) {
     
-    const localJobs = $("<ul>");
+    const localJobs = $("<ul>").addClass("jobList");
     const jobUrl = results[i].redirect_url;
-    console.log(jobUrl); //displays the response.results[i].title
+    console.log(jobUrl); //displays the website in the results
     
-    const jobSite = $("<a>").text(results[i].title).attr("href", jobUrl);
-    localJobs.append(jobSite);
-    $("#jobs").append(localJobs);
-    // results.attr(); 
+    const jobDiv = $("<div>").addClass("jobDiv");
+    const jobInfo = results[i].description;
+    jobDiv.append(jobInfo);
+    console.log(jobInfo); //will provide job details if you're interested
+    
 
+    const jobSite = $("<div>").text(results[i].title).addClass("jobSite"); //variable to give the localJobs **removed .attr("href", jobUrl);
+    localJobs.append(jobSite).append(jobDiv);
+    
+    $("#jobs").append(localJobs);
+
+    
+}
+const coll = $(".jobSite");
+//jobSite is our "collapsible", jobInfo is our "content"
+
+for(let i = 0; i < coll.length; i++){
+    $('.jobDiv').css("display", "none");
+    coll[i].addEventListener("click", function(){
+        this.classList.toggle("active");
+        const content = this.nextElementSibling;
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    })
 }
 
 
-// console.log(jobDiv);
-
-// jobDiv.append("#job-tab");
-// localJobs.append(jobDiv);
 
 })
-    
-                    $("#search").val(" ");
-    // }
-}); 
-    
-// });
+
+})
