a0ef80993a0da99dad7fa43329b98ff5b41336ec
reb2039607 <reb.aguayo@gmail.com>
2019-07-07 15:42:39 -0700

    added apply button with job description

--- javascript/jobs.js
+++ javascript/jobs.js
@@ -15,50 +15,57 @@ $(document).on("click", ".search-btn", function () {
     })
         .then(function (response) {
 
-console.log(response)//testing response...it works and pulls info from api
+            console.log(response)//testing response...it works and pulls info from api
 
-//new variable to use
-let results = response.results;
-console.log(results);
+            //new variable to use
+            let results = response.results;
+            console.log(results);
 
-//looping the results
-for(let i = 0; i < results.length; i++) {
-    
-    const localJobs = $("<ul>").addClass("jobList");
-    const jobUrl = results[i].redirect_url;
-    console.log(jobUrl); //displays the website in the results
-    
-    const jobDiv = $("<div>").addClass("jobDiv");
-    const jobInfo = results[i].description;
-    jobDiv.append(jobInfo);
-    console.log(jobInfo); //will provide job details if you're interested
-    
+            //looping the results
+            for (let i = 0; i < results.length; i++) {
 
-    const jobSite = $("<div>").text(results[i].title).addClass("jobSite"); //variable to give the localJobs **removed .attr("href", jobUrl);
-    localJobs.append(jobSite).append(jobDiv);
-    
-    $("#jobs").append(localJobs);
+                const localJobs = $("<ul>").addClass("jobList");
+                const jobUrl = results[i].redirect_url;
+                console.log(jobUrl); //displays the website in the results
 
-    
-}
-const coll = $(".jobSite");
-//jobSite is our "collapsible", jobInfo is our "content"
-
-for(let i = 0; i < coll.length; i++){
-    $('.jobDiv').css("display", "none");
-    coll[i].addEventListener("click", function(){
-        this.classList.toggle("active");
-        const content = this.nextElementSibling;
-        if (content.style.display === "block") {
-            content.style.display = "none";
-        } else {
-            content.style.display = "block";
-        }
-    })
-}
+                const jobDiv = $("<div>").addClass("jobDiv");
+                const jobInfo = results[i].description;
+                jobDiv.append(jobInfo);
+                //console.log(jobInfo); //will provide job details if you're interested
 
 
+                const jobSite = $("<div>").text(results[i].title).addClass("jobSite").attr("href", jobUrl); //variable to give the localJobs **removed .attr("href", jobUrl);
+                localJobs.append(jobSite).append(jobDiv);
 
-})
+                $("#jobs").append(localJobs);
+
+
+            }
+            const coll = $(".jobSite");
+        
+            const apply = $("<button>");
+            apply.text("Apply!");
+
+
+            for (let i = 0; i < coll.length; i++) {             
+                $('.jobDiv').css("display", "none");
+                coll[i].addEventListener("click", function () {
+                    this.classList.toggle("active");
+                    const content = this.nextElementSibling;
+                    if (content.style.display === "block") {
+                        content.style.display = "none";
+                    } else {
+                        content.style.display = "block";
+                    }
+                })
+            }
+            $(".jobDiv").append(apply);
+
+
+            
+
+
+
+        })
 
 })
