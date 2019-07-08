9a0c430debe960dd09457300c85d712e88714f8a
MagicMayo <mike.mayoii@gmail.com>
2019-07-08 15:41:40 -0700

    finished conditionals for changing background based on weather conditions

--- javascript/life.js
+++ javascript/life.js
@@ -24,9 +24,37 @@ $(document).on('click', '.search-btn', function(event){
         const average = response.data.ClimateAverages[0].month;
         const currentCond = response.data.current_condition[0];
         const forecast = response.data.weather;
+        const currentDesc = currentCond.weatherCode;
 
+        console.log(currentDesc)
         // Prepending current conditions so it's seen first
         $('#weather').prepend('<p>Current Temp: ' + currentCond.temp_F + '</p>')
+
+        // Setting the background image based on the current weather description for the currently searched city
+        if (currentDesc == 122 || currentDesc == 119){
+            $('body').css('background-image', 'url(../images/cloudy.gif)');
+        }
+        else if (currentDesc == 266 || currentDesc == 263 || currentDesc == 293 || currentDesc == 296 || currentDesc == 299 || currentDesc == 302 || currentDesc == 305 || currentDesc == 308 || currentDesc == 353 || currentDesc == 356 || currentDesc == 359 || currentDesc == 176){
+            $('body').css('background-image', 'url(../images/rain.gif)');
+        }
+        else if (currentDesc == 116){
+            $('body').css('background-image', 'url(../images/partlycloudy.gif)');
+        }
+        else if (currentDesc == 113){
+            $('body').css('background-image', 'url(../images/sunny.jpg)');
+        }
+        else if (currentDesc == 200 || currentDesc == 386 || currentDesc == 389){
+            $('body').css('background-image', 'url(../images/storm.gif)');
+        }
+        else if (currentDesc == 248 || currentDesc == 260 || currentDesc == 143){
+            $('body').css('background-image', 'url(../images/foggy.jpg)');
+        }
+        else if (currentDesc == 392 || currentDesc == 395 || currentDesc == 368 || currentDesc == 371 || currentDesc == 338 || currentDesc == 332 || currentDesc == 335 || currentDesc == 329 || currentDesc == 326 || currentDesc == 323 || currentDesc == 230 || currentDesc == 227 || currentDesc == 179){
+            $('body').css('background-image', 'url(../images/snow.gif)');
+        }
+        else if (currentDesc == 374 || currentDesc == 377 || currentDesc == 350 || currentDesc == 365 || currentDesc == 362 || currentDesc == 320 || currentDesc == 317 || currentDesc == 311 || currentDesc == 314 || currentDesc == 284 || currentDesc == 281 || currentDesc == 182 || currentDesc == 185){
+            $('body').css('background-image', 'url(../images/hail.gif)');
+        }
 
         // Loop to populate monthly average table
         for (let i=0; i<12;i++){
